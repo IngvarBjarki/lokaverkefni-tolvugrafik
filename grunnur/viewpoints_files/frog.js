2aae353834b56f024ae3e25817f3eadb0b4ca894
IngvarBjarki <ingvarbjarki@gmail.com>
2016-04-10 13:40:31 +0000

    buinn ad vera breyta adalfalinu og frog klasinn kominn inn hef ekki profad veit ekki hvort tad virkar

---
+++ grunnur/viewpoints_files/frog.js
@@ -0,0 +1,35 @@
+function frog(){
+
+    this.frogXPos = 0.0;
+    this.frogYPos = 0.0;
+    this.frogSize = 5.0;
+    this.frogAngle = 90;
+    this.frogDirectionX = Math.cos(radians(this.frogAngle)); // til þess hann fari ekki i x at þegar hann a ad fara i y att
+    this.frogDirectionY = Math.sin(radians(this.frogAngle)); // -||-
+    this.frogStepSize = 1.5;
+    this.frogWidth = this.size;
+    this.frogLenegth = this.size;
+
+}
+
+
+frog.prototype.update = function(){
+
+// utbum switch settningu fyrir hvada orvatakkar eru valdir
+
+
+};
+
+frog.prototype.render = function(){
+    var mv=vec4;
+    mv = mult(mv, translate(this.frogXPos, this.frogYPos, this.frogWidth));
+    // ef vid viljum rotadea ta kemur tad her
+    mv = mult(mv, scalem(this.frogSize, this.frogSize, this.frogSize));
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.vertexAttribPointer(vPosition, 3, gl.FLOAT, false, 0, 0);
+    gl.uniform4fv(colorLoc, GREEN);
+    gl.uniformMatrix4fv(myLoc, false, flatten(mv));
+    gl.drawArrays(gl.TRIANGLES, 0, numCubeVertices);
+
+};
--- grunnur/viewpoints_files/viewpoints.js
+++ grunnur/viewpoints_files/viewpoints.js
@@ -33,16 +33,40 @@ var groundVertices = [
 
 ];
 
+// fyrir kassan a.k.a froskinn
+var squareVertices = [
+    // front side:
+    vec3( -0.5,  0.5,  0.5 ), vec3( -0.5, -0.5,  0.5 ), vec3(  0.5, -0.5,  0.5 ),
+    vec3(  0.5, -0.5,  0.5 ), vec3(  0.5,  0.5,  0.5 ), vec3( -0.5,  0.5,  0.5 ),
+    // right side:
+    vec3(  0.5,  0.5,  0.5 ), vec3(  0.5, -0.5,  0.5 ), vec3(  0.5, -0.5, -0.5 ),
+    vec3(  0.5, -0.5, -0.5 ), vec3(  0.5,  0.5, -0.5 ), vec3(  0.5,  0.5,  0.5 ),
+    // bottom side:
+    vec3(  0.5, -0.5,  0.5 ), vec3( -0.5, -0.5,  0.5 ), vec3( -0.5, -0.5, -0.5 ),
+    vec3( -0.5, -0.5, -0.5 ), vec3(  0.5, -0.5, -0.5 ), vec3(  0.5, -0.5,  0.5 ),
+    // top side:
+    vec3(  0.5,  0.5, -0.5 ), vec3( -0.5,  0.5, -0.5 ), vec3( -0.5,  0.5,  0.5 ),
+    vec3( -0.5,  0.5,  0.5 ), vec3(  0.5,  0.5,  0.5 ), vec3(  0.5,  0.5, -0.5 ),
+    // back side:
+    vec3( -0.5, -0.5, -0.5 ), vec3( -0.5,  0.5, -0.5 ), vec3(  0.5,  0.5, -0.5 ),
+    vec3(  0.5,  0.5, -0.5 ), vec3(  0.5, -0.5, -0.5 ), vec3( -0.5, -0.5, -0.5 ),
+    // left side:
+    vec3( -0.5,  0.5, -0.5 ), vec3( -0.5, -0.5, -0.5 ), vec3( -0.5, -0.5,  0.5 ),
+    vec3( -0.5, -0.5,  0.5 ), vec3( -0.5,  0.5,  0.5 ), vec3( -0.5,  0.5, -0.5 )
+];
+
+squareVertices = 36;
+
 window.onload = function init()
 {
     canvas = document.getElementById( "gl-canvas" );
-    
+
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0, 0.5, 1, 0.5 );
-    
+
     gl.enable(gl.DEPTH_TEST);
 
     //
@@ -50,11 +74,16 @@ window.onload = function init()
     //
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
-    
+
     // VBO for the ground
     groundBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, groundBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(groundVertices), gl.STATIC_DRAW );
+
+    //Buffer fyrir froskinn
+    frogBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, frogBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(frog))
 
 
     vPosition = gl.getAttribLocation( program, "vPosition" );
@@ -62,7 +91,7 @@ window.onload = function init()
     gl.enableVertexAttribArray( vPosition );
 
     colorLoc = gl.getUniformLocation( program, "fColor" );
-    
+
     mvLoc = gl.getUniformLocation( program, "modelview" );
 
     // set projection
@@ -78,7 +107,7 @@ window.onload = function init()
 function ground( x, y, size, mv ) {
 
     gl.uniform4fv( colorLoc, GREEN );
-    
+
     mv = mult( mv, translate( x, y, size/2 ) );
     mv = mult( mv, scalem( size, size, size ) );
 
@@ -88,13 +117,13 @@ function ground( x, y, size, mv ) {
     gl.uniformMatrix4fv(mvLoc, false, flatten(mv));
     gl.drawArrays( gl.TRIANGLES, 0, numCubeVertices );
 }
-    
+
 
 // draw the ground
 function drawGround( mv ,size) {
 
     gl.uniform4fv( colorLoc, GREEN );
-    
+
     mv = mult( mv, translate( -100, 0, size/2 ) );
     mv = mult( mv, scalem( size, size, size ) );
 
@@ -106,7 +135,7 @@ function drawGround( mv ,size) {
 
 }
 
-    
+
 
 function render()
 {
@@ -118,8 +147,8 @@ function render()
     // Distant and stationary viewpoint
     mv=mult(mv,scalem(100,100,0));
 
-    
-    
+
+
     requestAnimFrame( render );
 }
 
